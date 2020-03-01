3d2c46be630d1fc97129a791d3baa278ec192df9
fawazzist <fawas5027@gmail.com>
2020-03-01 20:11:59 +0530

    Flutter World Time Application Completed error free

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,44 +11,75 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context).settings.arguments;
+    data = data.isNotEmpty ? data:ModalRoute.of(context).settings.arguments;
     print(data);
+
+    String bgimage = data['isdaytime'] ? 'day.png' : 'night.png';
+    Color bgcolor = data['isdaytime'] ? Colors.blue : Colors.indigo;
 
 
     return Scaffold(
+      backgroundColor: bgcolor,
       body: SafeArea(
-        child:Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-              ),
-             SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
-                    style:TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
+        child:Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgimage'),
+              fit: BoxFit.cover,
+
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () async{
+                    dynamic result = await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location' : result['location'],
+                        'isdaytime':result['isdaytime'],
+                        'flag':result['flag']
+                      };
+                    });
+                    },
+                  icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                    style: TextStyle(
-                      fontSize: 60.0,
+                  label: Text(
+                      'Edit Location',
+                      style: TextStyle(
+                        color: Colors.grey[300],
+                      ),
+                  ),
+                ),
+               SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style:TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
                     ),
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                      style: TextStyle(
+                        fontSize: 60.0,
+                        color: Colors.white,
+                      ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
